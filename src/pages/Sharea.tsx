7f0202cd789bddacac2684942f9f40e874620d59
88bones <binitmaharjan2@gmail.com>
2026-07-30 00:15:11 +0545

    clear text

--- src/pages/Sharea.tsx
+++ src/pages/Sharea.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import { Trash, X } from "lucide-react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Sharea = () => {
   const { params } = useParams();
-  console.log(params);
+  // console.log(params);
+
+  const [text, setText] = useState<string>("");
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
 
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden relative">
       <textarea
-        // value={text}
-        // onChange={handleChange}
-        placeholder="Type your plain text here..."
+        value={text}
+        onChange={handleChange}
+        placeholder="Type your text here..."
         className="w-full px-4 border border-r-4 border-white rounded text-white font-light min-h-dvh bg-cyan"
       />
+
+      <div
+        className="text-white absolute bottom-20 right-10 bg-mint p-2 rounded-full shadow-lg shadow-teal hover:cursor-pointer"
+        onClick={() => setText("")}
+      >
+        <Trash size={24} />
+      </div>
     </div>
   );
 };
